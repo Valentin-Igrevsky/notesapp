3e1cb1b8a5668348e329a6fc3eae1e840aa88509
Valentin-Igrevsky <valentin.igrevsky@gmail.com>
2025-04-06 16:31:52 +0300

    DB UPD

--- server/src/main/java/org/server/database/Database.java
+++ server/src/main/java/org/server/database/Database.java
@@ -22,27 +22,33 @@ public class Database {
         }
     }
 
-    public boolean createUser(String name, String surname, String username, String password) throws SQLException {
+    public int createUser(String name, String surname, String username, String password) throws SQLException {
         String checkQuery = "SELECT COUNT(*) FROM users WHERE username = ?";
         PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
         checkStmt.setString(1, username);
         ResultSet rs = checkStmt.executeQuery();
 
         if (rs.next() && rs.getInt(1) > 0) {
-            return false;
+            return -1;
         }
 
         String query = "INSERT INTO users(username, password, name, surname) VALUES(?,?,?,?)";
         PreparedStatement pstmt = conn.prepareStatement(query);
-        pstmt.setString(1,   username);
+        pstmt.setString(1, username);
         pstmt.setString(2, password);
         pstmt.setString(3, name);
         pstmt.setString(4, surname);
         pstmt.executeUpdate();
-        return true;
+
+        ResultSet generatedKeys = pstmt.getGeneratedKeys();
+        if (generatedKeys.next()) {
+            return generatedKeys.getInt(1);
+        } else {
+            throw new SQLException("Creating user failed, no ID obtained.");
+        }
     }
 
-    public void addNote(@NotNull Note note) throws SQLException {
+    public int addNote(@NotNull Note note) throws SQLException {
         if (note.getId() > 0) {
             String checkQuery = "SELECT COUNT(*) FROM notes WHERE id = ?";
             PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
@@ -51,7 +57,7 @@ public class Database {
 
             if (rs.next() && rs.getInt(1) > 0) {
                 updateNote(note);
-                return;
+                return note.getId();
             }
         }
 
@@ -63,6 +69,13 @@ public class Database {
         pstmt.setString(4, note.getTitle());
         pstmt.setInt(5, note.getOwnerID());
         pstmt.executeUpdate();
+
+        ResultSet generatedKeys = pstmt.getGeneratedKeys();
+        if (generatedKeys.next()) {
+            return generatedKeys.getInt(1);
+        } else {
+            throw new SQLException("Inserting note failed, no ID obtained.");
+        }
     }
 
     public Note getNoteById(int noteId) throws SQLException {
@@ -112,16 +125,31 @@ public class Database {
         return new User(rs.getInt("id"), rs.getString("name"), rs.getString("surname"), rs.getString("password"), rs.getString("username"));
     }
 
-    public void updateUser(User user) throws SQLException {
-        String query = "UPDATE users SET name = ?, surname = ?, username = ?, password = ? WHERE id = ?";
-        PreparedStatement pstmt = conn.prepareStatement(query);
+    public User updateUser(User user) throws SQLException {
+        String checkQuery = "SELECT id FROM users WHERE username = ?";
+        PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
+        checkStmt.setString(1, user.getUsername());
+        ResultSet rs = checkStmt.executeQuery();
+
+        if (rs.next()) {
+            int existingUserId = rs.getInt("id");
+            if (existingUserId != user.getId()) {
+                return null;
+            }
+        }
+
+        String updateQuery = "UPDATE users SET name = ?, surname = ?, username = ?, password = ? WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(updateQuery);
         pstmt.setString(1, user.getName());
         pstmt.setString(2, user.getSurname());
         pstmt.setString(3, user.getUsername());
         pstmt.setString(4, user.getPassword());
         pstmt.setInt(5, user.getId());
         pstmt.executeUpdate();
+
+        return user;
     }
+
 
     public void updateNote(Note note) throws SQLException {
         String query = "UPDATE notes SET create_date = ?, last_modified = ?, text = ?, title = ?, owner_id = ? WHERE id = ?";
